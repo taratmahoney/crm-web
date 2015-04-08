5cbd10695edefd50bc1174ff013486fe5e554b2e
Tara Mahoney <tara.mahoney@me.com>
2015-04-08 15:14:03 -0400

    built out form to create new contacts

--- crm2.rb
+++ crm2.rb
@@ -2,13 +2,16 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
+#routes
+
 get "/" do 
   @crm_app_name = "My CRM"
   erb :index  
 end
 
 get "/contacts" do
-  @contacts = []
   @contacts << Contact.new("Tara", "Mahoney", "tara.mahoney@me.com", "Developer")
   @contacts << Contact.new("Taylor", "Scollon", "taylorscollon@gmail.com", "Designer")
   @contacts << Contact.new("Mollie", "Anderson", "mollieaanderson@gmail.com", "Marketing")
@@ -17,5 +20,5 @@ get "/contacts" do
 end
 
 get "/contacts/new" do
-  erb :new_contacts
+  erb :contacts_new
 end
