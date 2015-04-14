96828080d8d3411a85c83457f7a9071f739ae5ef
Tara Mahoney <tara.mahoney@me.com>
2015-04-14 15:53:56 -0400

    Added the ability to find contacts

--- crm2.rb
+++ crm2.rb
@@ -3,6 +3,8 @@ require_relative 'contact'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+contact = $rolodex.find(1)
 
 #routes
 
@@ -25,4 +27,9 @@ post "/contacts" do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
+end
+
+get "/contacts/1" do
+  @contact = $rolodex.find(1)
+  erb :show_contact
 end
--- rolodex.rb
+++ rolodex.rb
@@ -3,12 +3,16 @@ class Rolodex
 
   def initialize
     @contacts = []
-    @id = 1000
+    @id = 1
   end
 
   def add_contact(contact)
     contact.id = @id
     @contacts << contact
     @id += 1
+  end
+
+  def find(contact_id)
+    @contacts.find { |contact| contact.id == contact_id} #should return the first element that matches the condition in the block
   end
 end
